59b9d53514bcdc9042ce946fb90e0407417670d1
manossaratsis <manos.saratsis@nrg.com>
2017-01-29 10:04:08 -0800

    Adding mobile support

---
+++ content/js/navbar.js
@@ -0,0 +1,8 @@
+function openNav() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
