6ba3013810b41e7048337ed3051d74ac7b67005f
Manos Saratsis <manos.saratsis@gmail.com>
2017-02-02 07:54:03 -0800

    Working on blog functionality

---
+++ content/posts/2017-02-02-Hello-World.md
@@ -0,0 +1,7 @@
+---
+title: "Hello World"
+created_at: 2012-02-10 09:00:00 +0000
+kind: article
+---
+
+Welcome to my blog.
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,1 +1,4 @@
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Tagging
 include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::LinkTo
