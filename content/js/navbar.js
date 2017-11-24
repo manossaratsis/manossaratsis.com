59a9b52f1d53c7d8fa7d0419eac302b1862628b2
Manos Saratsis <manos.saratsis@gmail.com>
2017-11-23 17:42:40 -0800

    Improving navbar experience

--- content/js/navbar.js
+++ content/js/navbar.js
@@ -1,27 +1,30 @@
-function openNav() {
-    var x = document.getElementById("navbar");
-    if (x.className === "nav") {
-        x.className += " responsive";
+var openNav = function() {
+    var navMenu = document.querySelector('.nav-menu');
+    var navButton = document.querySelector('.nav-button');
+    if (navMenu.className === 'nav-menu') {
+        navMenu.className += ' responsive';
+        navButton.src = '/img/exit.svg';
     } else {
-        x.className = "nav";
+        navMenu.className = 'nav-menu';
+        navButton.src = '/img/menu.svg';
     }
-}
+};
 
 var highlightCurrentLink = function() {
-    var navLinks = document.querySelectorAll(".nav li a:not(.logo)");
-    var navPath = window.location.origin + "/" +
-                  window.location.pathname.split("/")[1];
-    if (!navPath.endsWith("/")) {
-        navPath += "/";
+    var navLinks = document.querySelectorAll('.nav-menu li a:not(.logo)');
+    var navPath = window.location.origin + '/' +
+                  window.location.pathname.split('/')[1];
+    if (!navPath.endsWith('/')) {
+        navPath += '/';
     }
     for (var i = 0; i < navLinks.length; i++) {
         if (navPath === navLinks[i].href) {
-            if (navLinks[i].className == "") {
-                navLinks[i].classList.add("currentLink");
+            if (navLinks[i].className == '') {
+                navLinks[i].classList.add('currentLink');
             }
             return;
         }
     }
 };
 
-window.addEventListener("load", highlightCurrentLink);
+window.addEventListener('load', highlightCurrentLink);
