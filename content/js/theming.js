972bc8bf71c9646b82c8261d897021686aa7142b
Manos Saratsis <manos.saratsis@gmail.com>
2022-09-21 17:26:40 -0400

    Adding gradients

--- content/js/theming.js
+++ content/js/theming.js
@@ -5,7 +5,6 @@ let background = document.querySelector('.background');
 if (background) {
   background.style['filter'] = 'blur(8px)';
   background.style['--webkit-filter'] = 'blur(8px)';
-  background.style['opacity'] = '0.6';
 }
 
 // Set the accent color
