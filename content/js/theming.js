c3c23a31f4f49f8ca4909b26fb4e82fb0b638fae
Manos Saratsis <manos.saratsis@gmail.com>
2022-09-21 16:42:37 -0400

    Adding vibrant.js

--- content/js/theming.js
+++ content/js/theming.js
@@ -1,7 +1,16 @@
+let root = document.documentElement;
 let background = document.querySelector('.background');
 
+// Edit the background image in the home page
 if (background) {
   background.style['filter'] = 'blur(8px)';
   background.style['--webkit-filter'] = 'blur(8px)';
   background.style['opacity'] = '0.6';
 }
+
+// Set the accent color
+Vibrant.from('/img/home.jpg').getPalette(function(err, palette) {
+  let vibrantColor = palette.Vibrant._rgb;
+  let vibrantColorRGB = ''.concat(vibrantColor[0], ', ', vibrantColor[1], ', ', vibrantColor[2]);
+  root.style.setProperty('--accent', vibrantColorRGB);
+});
