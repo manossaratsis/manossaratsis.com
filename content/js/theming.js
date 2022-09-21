6ee95f964933811ead2442e140c169c79638434e
Manos Saratsis <manos.saratsis@gmail.com>
2022-09-21 15:50:34 -0400

    Theming

---
+++ content/js/theming.js
@@ -0,0 +1,7 @@
+let background = document.querySelector('.background');
+
+if (background) {
+  background.style['filter'] = 'blur(8px)';
+  background.style['--webkit-filter'] = 'blur(8px)';
+  background.style['opacity'] = '0.6';
+}
